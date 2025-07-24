e2d060797d9ed9074e78a378b37edb0fa0ccb87a
niko <niko@DESKTOP-BN69L8N>
2025-07-25 00:07:37 +0200

    a5

--- Source.cpp
+++ Source.cpp
@@ -66,13 +66,14 @@ int main()
 			reached1000000000 = true;
 		}
 
-		// Optional: show total grains for next square (debug info)
-		cout << '\t' << "Total grains for next square (square " << i + 1 << "): " << totalRice << '\n';
+		
 
 		
 		
 
 		totalRice *= 2;
+		// Optional: show total grains for next square (debug info)
+		cout << '\t' << "Total grains for next square (square " << i + 1 << "): " << totalRice << '\n';
 		//cout << '\t' << "totalgrains value for square: " << i + 1 << "  are:" << '\t' << totalRice << "      loop number:" << i << '\n';
 
 	}//loop end
